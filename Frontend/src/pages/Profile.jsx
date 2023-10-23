8f54316d9bd4a89bf89d721f084ddff1bfb63ea8
gaurav7477 <bairagi7477@gmail.com>
2023-10-23 10:24:10 +0530

    completed

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -1,17 +1,18 @@
 import React, { useContext } from "react";
 import { Context } from "../main";
 import Loader from "../components/Loader";
-import "./profile.css"
 const Profile = () => {
-    const { isAuthenticated, loading, user } = useContext(Context);
+    const { loading, user } = useContext(Context);
 
     return loading ? (
         <Loader />
     ) : (
         <div className="profile">
             <h1>My Profile</h1>
-            <h3>Full Name - {user?.name}</h3>
-            <h3>Email - {user?.email}</h3>
+            <div className="user">
+                <h2>Name: {user.name}</h2>
+                <h2>Email: {user.email}</h2>
+            </div>
         </div>
     );
 };
